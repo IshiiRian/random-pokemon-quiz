2e1b0668593a57c2fdf9539e51f51aa2dea57a69
Ishii Rian <rianishii0612@gmail.com>
2024-08-26 09:35:11 +0900

    develop config

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,5 @@ import adapter from '@sveltejs/adapter-vercel';
 export default {
   kit: {
     adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '' : '',
-    },
   },
 };
